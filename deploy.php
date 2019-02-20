13e718af66cd89449827d25c1bb26ecda432a22a
Caspar Kirsch <mail@casparkirsch.de>
2019-02-20 19:34:53 +0100

    Remove install-composer task
    Deployer installs composer by default on deploy

--- deploy.php
+++ deploy.php
@@ -30,11 +30,3 @@ task('deploy', [
 ])->desc('Deploy your project');
 
 after('deploy', 'success');
-
-task('install-composer', function () {
-    run("php -d allow_url_fopen=On -r \"readfile('https://getcomposer.org/installer');\" > composer-setup.php;");
-    run("php -d allow_url_fopen=On composer-setup.php");
-    run("php -r \"unlink('composer-setup.php');\"");
-    run("echo \"alias composer=/usr/bin/php -d allow_url_fopen=On /usr/home/\$USER/composer.phar\" >> ~/.bashrc");
-
-})->desc('Install composer on a host');
